d40e5e911cfe2f80efb51b2471eeb1597baf1f2a
strykerin <dacosta.pereirafabio@gmail.com>
2022-08-06 13:59:25 +0400

    pass tests for CoinFlip.sol

--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -1,8 +1,12 @@
 import { expect } from "chai";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
-const helper = async (victim: any, attacker: any) => {
+const helper = async (victim: Contract, attacker: Contract) => {
   // add code here that will help you pass the test
+  for (let i = 0; i < 10; i++) {
+    await attacker.hackContract();
+  }
 };
 
 export default helper;
